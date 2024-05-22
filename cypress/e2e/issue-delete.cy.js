923eea6fb39f864977eb5071c136f91a968e1875
Alexandre Jaffelin <kagoulecodeur@gmail.com>
2024-05-22 14:12:43 +0100

    22/05/2024

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -7,35 +7,42 @@ describe('Issue deleting', () => {
     });
 
     it('Test Case 1: Issue Deletion', () => {
+        // Assert list is visible
         cy.get('[data-testid="board-list:backlog"]')
         .should('be.visible')
         .and('have.length', '1')
         .within(() => {
-            // Assert that this list contains 3 issues and first element with tag p has specified text
+            // Assert that this list contains 4 issues 
             cy.get('[data-testid="list-issue"]')
             .should('have.length', '4') 
         });
 
+        // Click on issue to be deleted
         cy.contains('This is an issue of type: Task.').click();
 
+        // Click on the trash icon to delete said issue
         cy.get('[data-testid="modal:issue-details"]').within(() => {
             cy.get('[data-testid="icon:trash"]').click();
 
         });
 
+        // Confirm the deletion of said issue
         cy.get('[data-testid="modal:confirm"]').within(() => {
             cy.contains('Delete issue').click();
         });
 
+        // wait 5sec 
         cy.wait(5000);
 
+        // After waiting, reload
         cy.reload();
 
+        // Assert that list is visible
         cy.get('[data-testid="board-list:backlog"]')
         .should('be.visible')
         .and('have.length', '1')
         .within(() => {
-            // Assert that this list contains 3 issues and first element with tag p has specified text
+            // Assert that this list contains 3 issues 
             cy.get('[data-testid="list-issue"]')
             .should('have.length', '3') 
         });
