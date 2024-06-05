673540f95af495dab7f25bb6a7fbc23e783043d8
Alexandre Jaffelin <kagoulecodeur@gmail.com>
2024-06-05 17:17:26 +0100

    05/06/2024

--- cypress/e2e/issue-comments.cy.js
+++ cypress/e2e/issue-comments.cy.js
@@ -69,7 +69,10 @@ describe('Issue comments creating, editing and deleting', () => {
             .should('not.exist');
     });
 
-    it.only('Should add, update and delete a comment successfully', () => {
+    /* Assigment 5: Add, update, delete comments
+    05/06/2024 */
+
+    it('Should add, update and delete a comment successfully', () => {
         const addComment = 'Adding a proper comment to this comment section';
         const updateComment = 'Updating previous comment, isn\'t cool ?'
 
@@ -131,4 +134,75 @@ describe('Issue comments creating, editing and deleting', () => {
             .contains(updateComment)
             .should('not.exist');
     });
+
+
+    // Using functions to add, update and delete
+
+    const comment = "First comment using function"
+    const updatedComment = "Second comment updated for other function"
+    
+    function addingComment(comment) {
+        getIssueDetailsModal().within(() => {
+            cy.contains('Add a comment...')
+                .click();
+
+            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.contains('Add a comment...').should('exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', comment);
+        });
+    }
+
+    function updatingComment(comment, updatedComment) {
+        getIssueDetailsModal().within(() => {
+            cy.get('[data-testid="issue-comment"]')
+                .first()
+                .contains('Edit')
+                .click()
+                .should('not.exist');
+
+            cy.get('textarea[placeholder="Add a comment..."]')
+                .should('contain', comment)
+                .clear()
+                .type(updatedComment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.get('[data-testid="issue-comment"]')
+                .should('contain', 'Edit')
+                .and('contain', updatedComment);
+        });
+    }
+
+    function deletingComment(updatedComment) {
+        getIssueDetailsModal()
+        .find('[data-testid="issue-comment"]')
+        .contains('Delete')
+        .click();
+
+    cy.get('[data-testid="modal:confirm"]')
+        .contains('button', 'Delete comment')
+        .click()
+        .should('not.exist');
+
+    getIssueDetailsModal()
+        .find('[data-testid="issue-comment"]')
+        .should('not.contain', updatedComment);
+    }
+
+    it.only('Using functions, it should add, update and delete a comment successfully', () => {
+        const comment = "First comment using function"
+        const updatedComment = "Second comment updated for other function"
+        
+        addingComment(comment);
+        updatingComment(comment, updatedComment);
+        deletingComment(updatedComment)
+    });
+
 });
